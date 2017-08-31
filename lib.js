7bdb591b04c6842593c2a4e0ca920977aa47a84d
seeker105 <karavol27@gmail.com>
2017-08-31 16:03:15 -0500

    Separate nested reduce into separate function. Refactor to reduce lines of code

--- lib.js
+++ lib.js
@@ -32,26 +32,42 @@ const itemRepeater =
       Array(count).fill(itemName)
 
 /**
+ * Accepts an array of key-value pairs stored as separate arrays. Example:
+ * 
+ * [ ['detergent', 2], ['cookies', 3] ]
+ * 
+ * Returns 1 dimensional array in which each key appears the number of times
+ * specified in it's value. The above input would produce
+ * 
+ * ['detergent', 'detergent', 'cookies', 'cookies', 'cookies']
+ * 
+ * If a value from a key-value pair is not an integer the behavior is undefined
+ * @param {Array} entriesArray 
+ */
+const getCurrentItemList = (entriesArray) => 
+  entriesArray.reduce(
+    (itemStrings, currentItem) => {
+      let currentItemList = itemRepeater(currentItem[0])(currentItem[1])
+      return itemStrings.concat(currentItemList)
+    }, [])
+
+
+/**
  * should return an array of carts with each given customer's shopping list
  * as an array of items
  */
 const constructCarts =
   listings =>
-    customers => {
-      return customers.reduce(
+    customers => 
+      customers.reduce(
         (cartArray, currentCustomer) => {
-          let entriesArray = entries(currentCustomer.shoppingList)
           cartArray.push({
             customer: currentCustomer.name,
-            items: entriesArray.reduce(
-              (itemStrings, currentItem) => {
-                let currentItemList = itemRepeater(currentItem[0])(currentItem[1])
-                return itemStrings.concat(currentItemList)
-              }, [])
+            items: getCurrentItemList(entries(currentCustomer.shoppingList))
           })
           return cartArray
         }, [])
-    }
+    
 
 module.exports = {
   listing,
